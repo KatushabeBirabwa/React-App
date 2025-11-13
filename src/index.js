c77c56776317ae59434cd0a0d4af91720a0bf51f
KatushabeBirabwa <katu.birabwa@gmail.com>
2025-11-13 11:36:26 -0500

    Prepare for GitHub Pages deployment

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-// index.js
 import App from './App';
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.render(
-  <StrictMode>
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
