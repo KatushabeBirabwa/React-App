752e6a6ac8c715a848b4ea6abe34fb374ef1b50e
KatushabeBirabwa <katu.birabwa@gmail.com>
2025-11-13 20:56:02 -0500

    Fix App.js and index.js for GitHub Pages deployment

--- src/App.js
+++ src/App.js
@@ -1,28 +1,19 @@
 import './App.css';
 
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Route, Routes } from 'react-router-dom';
 
 import Home from "./Pages/Home/HomeScreen";
 import Navbar from "./Pages/Home/Navbar";
-import React from "react";
-import { StrictMode } from 'react';
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
-
-        <Router>
-          <div>
-            <Navbar />
-            <Routes>
-              <Route path="/" element={<Home /> } ></Route>
-              <Route path="*" element={<div> 404 Not Found</div>} ></Route>
-
-              </Routes>
-
-          </div>
-        </Router>
-
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<div>404 Not Found</div>} />
+      </Routes>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
+
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
